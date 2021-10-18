ac21a565a7216bcf96665cfa5ef38b60a422253e
Dawid Ciechowski <dawid.ciechowski@gmail.com>
2021-10-18 21:12:09 +0200

    Bugfix

--- main.py
+++ main.py
@@ -11,6 +11,5 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 bot = commands.Bot(command_prefix="Czerwony ", case_insensitive=False)
 
 bot.load_extension("cogs.bot_utilities")
-# bot.load_extension("cogs.music")
 bot.load_extension("cogs.youtube")
 bot.run(TOKEN)
